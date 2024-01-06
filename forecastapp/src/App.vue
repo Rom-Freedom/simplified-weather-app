95370bf428c5ec9a7b20cb4671041af9dbfe7d41
Rom-Freedom <romeo.dark@mail.ru>
2024-01-06 12:13:26 +0300

    create basis for the app

---
+++ forecastapp/src/App.vue
@@ -0,0 +1,25 @@
+<script>
+
+</script>
+
+<template>
+    <div class="wrapper">
+        <h1>Weather app</h1>
+        <p>Find out the weather in your city</p>
+    </div>
+</template>
+
+<style scoped>
+.wrapper {
+    width: 800px;
+    height: 400px;
+    border-radius: 50px;
+    padding: 50px 20px 20px 20px;
+    background: #1f0f24;
+    text-align: center;
+    color: #fff;
+}
+h1 {
+    margin-bottom: 20px;
+}
+</style>
